a0a28bc6df2fdf49448bb311ea158032d366d9fe
rijal <jeryzal333@gmail.com>
2022-03-30 16:12:14 +0700

    login, register form finished

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,31 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import router from "../router";
+import { auth } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "@firebase/auth";
 
 export default createStore({
-  state: {
-  },
+  state: {},
   mutations: {
+    SET_USER(state, user) {
+      state.user = user;
+    },
+    CLEAR_USER(state) {
+      state.user = null;
+    },
   },
   actions: {
+    async login({ commit }, details) {
+      const { email, password } = details;
+
+      try {
+        await signInWithEmailAndPassword(auth);
+      } catch (error) {}
+    },
+    async register({ commit }, details) {},
+    async logut({ commit }) {},
   },
-  modules: {
-  }
-})
+});
